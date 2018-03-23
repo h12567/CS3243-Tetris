7b71cb00d6d98eee56cb4cf862f4ebceb364960e
aguss787 <agus.sentosa787@gmail.com>
2018-03-23 13:18:14 +0800

    Add simulator

---
+++ src/Simulator.java
@@ -0,0 +1,26 @@
+public class Simulator {
+    private double blockageParam;
+    private double heightParam;
+    private double bumpinessParam;
+    private double clearedParam;
+
+    public Simulator(double[] gene) {
+        this.blockageParam = gene[0];
+        this.heightParam = gene[1];
+        this.bumpinessParam = gene[2];
+        this.clearedParam = gene[3];
+    }
+
+    public int pickMove(State s, int[][] legalMoves) {
+        return Logic.getBestMove(s, legalMoves, paramBlockage, paramAggregateHeight,
+            paramBumpiness, paramLinesCleared);
+    }
+
+    public int getScore() {
+        State s = new State();
+        while(!s.hasLost()) {
+            s.makeMove(pickMove(s,s.legalMoves()));
+        }
+        return s.getRowsCleared();
+    }
+}
