310f789ecf18a3249fb83cb470800edfee5c1f5c
lth08091998 <lth08091998@gmail.com>
2018-04-13 08:07:06 +0800

    Best gene so far

--- src/PlayerSkeleton.java
+++ src/PlayerSkeleton.java
@@ -3,7 +3,7 @@ public class PlayerSkeleton {
     // private double[] gene = {0.06545828543097637, 0.5753446174725069, 0.15964887748502163,
     //         -0.3896014639203992, 0.6981523610521604, 0};
 
-     private double[] gene = {-5.253225416166894,-8.33674819178308,-1.476473328855904,-8.041765548443676,-3.8904547245744556};
+     private double[] gene = {11.655255086638178,-7.1857935342862564,-0.39428956350580413,-4.7836948807868644,-3.454162493658023};
 
 
     //implement this function to have a working system
@@ -12,7 +12,7 @@ public class PlayerSkeleton {
     }
 
     public static void main(String[] args) {
-        
+
     System.out.println("GOOG VERSION");
         int t = 100;
         if (args.length == 1) {
@@ -24,13 +24,13 @@ public class PlayerSkeleton {
         PlayerSkeleton p = new PlayerSkeleton();
         while (!s.hasLost()) {
             s.makeMove(p.pickMove(s, s.legalMoves()));
-            s.draw();
-            s.drawNext(0, 0);
-            try {
-                Thread.sleep(t);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+            // s.draw();
+            // s.drawNext(0, 0);
+            // try {
+            //     Thread.sleep(t);
+            // } catch (InterruptedException e) {
+            //     e.printStackTrace();
+            // }
         }
         System.out.println("You have completed " + s.getRowsCleared() + " rows.");
     }
